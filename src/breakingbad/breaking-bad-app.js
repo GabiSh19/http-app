8234a6433b4947ffbfe44d47da31739255d67132
Gabi13st.h <gabs13.h@gmail.com>
2024-06-08 21:53:44 -0400

    realiza fetch a API

--- src/breakingbad/breaking-bad-app.js
+++ src/breakingbad/breaking-bad-app.js
@@ -1,3 +1,15 @@
+/**
+ * 
+ * @returns {Object} quote 
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch ('https://api.breakingbadquotes.xyz/v1/quotes/5');
+    console.log(res);
+
+}
+
+
 
 /**
  * 
@@ -8,4 +20,5 @@ export const BreakingbadApp = (element) => {
     document.querySelector('#app-title').innerHTML = 'Breakingbad App'
     element.innerText = 'Loading...'  
 
+    fetchQuote();
 };
