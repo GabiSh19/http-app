c2b7899a3e05f178ef02571b02b4fc775e26a48f
Gabi13st.h <gabs13.h@gmail.com>
2024-06-08 22:00:38 -0400

    Añade elementos HTML

--- src/breakingbad/breaking-bad-app.js
+++ src/breakingbad/breaking-bad-app.js
@@ -1,12 +1,15 @@
 /**
  * 
- * @returns {Object} quote 
+ * @returns {Promise<Object> } quote 
  */
 const fetchQuote = async() => {
 
     const res = await fetch ('https://api.breakingbadquotes.xyz/v1/quotes/5');
-    console.log(res);
+    // console.log(res);
+    const data = await res.json();
+    console.log(data[0]);
 
+    return data[0];
 }
 
 
@@ -15,10 +18,20 @@ const fetchQuote = async() => {
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = (element) => {
+export const BreakingbadApp = async (element) => {
 
     document.querySelector('#app-title').innerHTML = 'Breakingbad App'
     element.innerText = 'Loading...'  
+    // const quote = await fetchQuote();
 
-    fetchQuote();
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = () => {
+        
+    }
+
 };
