9c838c45aeb1955cb1fa3a25696a945a59b73113
Gabi13st.h <gabs13.h@gmail.com>
2024-06-11 21:50:52 -0400

    añade archivo user a carpeta models

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -9,7 +9,7 @@ export const loadUsersByPage = async ( page = 1 ) => {
 
     const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
     const res = await fetch(url);
-    const data = await res.json();
+    const {data} = await res.json();
 
     console.log(data);
 
