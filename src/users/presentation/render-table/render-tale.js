0d656e960a253955add410f297cea103611c4d03
Gabi13st.h <gabs13.h@gmail.com>
2024-06-12 21:05:34 -0400

    crea tabla donde se renderizarán los usuarios

---
+++ src/users/presentation/render-table/render-tale.js
@@ -0,0 +1,38 @@
+//Cuidado por que esto se va a cargar de manera global, se va a cargar hasta cuando nuestro componente sea necesario.
+import './render-table.css';
+
+let table;
+
+const createTable = () =>{
+    const table = document.createElement('table');
+    const tableHeaders = document.createElement('thead');
+    tableHeaders.innerHTML = `
+    <tr>
+        <th>#ID</th>
+        <th>Balance</th>
+        <th>FirstName</th>
+        <th>LastName</th>
+        <th>Active</th>
+        <th>Actions</th>
+    </tr>
+    `;
+    const tableBody = document.createElement('tbody');
+    table.append(tableHeaders, tableBody);
+    return table; 
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderTable = (element) => {
+
+    const users = usersStore.getUsers();
+
+    if (!table) {
+        table = createTable();
+        table.append
+    }
+
+
+}
